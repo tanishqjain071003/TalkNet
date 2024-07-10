c2572fafa8b38c1b803026218b4cf5c0c5b4dedc
tanishqjain071003 <135233096+tanishqjain071003@users.noreply.github.com>
2024-07-10 18:00:40 +0530

    Update Conversations.jsx

--- frontend/src/components/Sidebar/Conversations.jsx
+++ frontend/src/components/Sidebar/Conversations.jsx
@@ -1,10 +1,11 @@
 import {React,useState,useEffect} from 'react'
 import axios from "axios"
 import Conversation from './Conversation.jsx'
+import { useAuthContext } from '../../context/AuthContext.jsx'
 
 const Conversations = () => {
   	const [users,setUsers] = useState([]);
-	const url = "http://localhost:4000";
+	const {url} = useAuthContext();
 	const token = localStorage.getItem("token");
 
 	const getAllUsers = async ()=>{
