5bd69d85e73976e8ed28bc76611eecd7da9d97aa
tanishqjain071003 <135233096+tanishqjain071003@users.noreply.github.com>
2024-07-17 05:36:35 +0530

    Add files via upload

---
+++ frontend/src/hooks/useGetConversations.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+
+
+const useGetConversations = () => {
+	const [loading, setLoading] = useState(false);
+	const [conversations, setConversations] = useState([]);
+    const url = 'http://localhost:4000';
+    const token = localStorage.getItem("token")
+	useEffect(() => {
+        
+		const getConversations = async () => {
+			setLoading(true);
+			try {
+				const response = await axios({
+                    method:"get",
+                    url:url+`/api/users`,
+                    headers:{token},
+                })
+                
+				setConversations(response.data.filteredUsers);
+			} catch (error) {
+				toast.error(error.message);
+			} finally {
+				setLoading(false);
+			}
+		};
+
+		getConversations();
+	}, []);
+
+	return { loading, conversations };
+};
+export default useGetConversations;
