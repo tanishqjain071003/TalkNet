bbf3cf98dc9df1161c7d2c67abb4b3ee038722e7
tanishqjain071003 <135233096+tanishqjain071003@users.noreply.github.com>
2024-07-17 07:35:16 +0530

    Update Sidebar.jsx

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,18 +1,24 @@
 import React, { useEffect, useState } from 'react'
 import SearchInput from './SearchInput.jsx'
-import axios from 'axios'
 import Conversations from './Conversations.jsx'
 import LogoutButton from './LogoutButton.jsx'
+import Profile from './Profile.jsx'
 
 const Sidebar = () => {
 
   return (
-    <div className='border-r border-slate-500 p-4 flex flex-col'>
-			<SearchInput />
-			<div className='divider px-3'></div>
-			<Conversations />
-			<LogoutButton />
+	<div className='flex flex-col'>
+		<div className='border-r border-slate-500 p-4 flex flex-col'>
+				<SearchInput />
+				<div className='divider px-3'></div>
+				<Conversations />
+				<LogoutButton />	
 		</div>
+		<div className='mt-auto p-4 flex flex-col-reverse'>
+			<Profile />
+		</div>
+	</div>
+    
   )
 }
 
