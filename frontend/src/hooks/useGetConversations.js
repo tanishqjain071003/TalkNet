b34c2f4e6ccaf77118d18a385b2fa50ab39822c3
tanishqjain071003 <135233096+tanishqjain071003@users.noreply.github.com>
2024-07-17 05:46:35 +0530

    Update useGetConversations.js

--- frontend/src/hooks/useGetConversations.js
+++ frontend/src/hooks/useGetConversations.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
+import useAuthContext from '../context/AuthContext.jsx'
 
 
 const useGetConversations = () => {
