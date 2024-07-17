22064b2d02e17accfd7e7ecf5854f38de6edc574
tanishqjain071003 <135233096+tanishqjain071003@users.noreply.github.com>
2024-07-17 07:37:32 +0530

    Update and rename userRouter.js to userRoute.js

---
+++ backend/routes/userRoute.js
@@ -0,0 +1,20 @@
+import { signUp , logIn,currentUser} from "../controllers/userController.js";
+import express from "express"
+import multer from 'multer'
+
+const userRoute = express.Router()
+
+const storage = multer.diskStorage({
+    destination: 'uploads',
+    filename: (req, file, cb) => {
+        return cb(null,`${Date.now()}${file.originalname}`);
+    }
+})
+
+const upload = multer({ storage: storage})
+
+userRoute.post('/signup',upload.single('image'),signUp);
+userRoute.post('/login',logIn);
+userRoute.get('/thisUser',currentUser);
+
+export default userRoute
