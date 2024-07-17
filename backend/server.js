ad7478aa491c44fd4bfdcab00b99f0b2a3ea87b7
tanishqjain071003 <135233096+tanishqjain071003@users.noreply.github.com>
2024-07-17 07:37:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors())
 
+app.use("/images", express.static(path.join(__dirname, 'uploads')));
 app.use("/api/user",userRouter)
 app.use("/api/message",messageRoute)
 app.use("/api/users",allUsersRoute)
