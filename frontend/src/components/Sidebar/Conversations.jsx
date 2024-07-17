cce5a8608d210d1638142433ba93eb13bebbd3db
tanishqjain071003 <135233096+tanishqjain071003@users.noreply.github.com>
2024-07-17 05:34:20 +0530

    Update Conversations.jsx

--- frontend/src/components/Sidebar/Conversations.jsx
+++ frontend/src/components/Sidebar/Conversations.jsx
@@ -1,35 +1,39 @@
 import {React,useState,useEffect} from 'react'
 import axios from "axios"
 import Conversation from './Conversation.jsx'
-import { useAuthContext } from '../../context/AuthContext.jsx'
+import useGetConversations from '../../hooks/useGetConversations.js'
 
 const Conversations = () => {
   	const [users,setUsers] = useState([]);
-	const {url} = useAuthContext();
-	const token = localStorage.getItem("token");
+	// const url = "http://localhost:4000";
+	// const token = localStorage.getItem("token");
 
-	const getAllUsers = async ()=>{
-		const response = await axios({
-                        method: 'get',
-                        url: url + '/api/users',
-                        headers: {token}, 
-                      });
-		if(response.data.success){
-			setUsers(response.data.filteredUsers);
-		}
-	}
+	// const getAllUsers = async ()=>{
+	// 	const response = await axios({
+    //                     method: 'get',
+    //                     url: url + '/api/users',
+    //                     headers: {token}, 
+    //                   });
+	// 	if(response.data.success){
+	// 		setUsers(response.data.filteredUsers);
+	// 	}
+	// }
+	const {loading,conversations} = useGetConversations();
 
-	useEffect(()=>{
-		if (token) {
-			getAllUsers();
-		}	  
-	},[token])
 
+	// useEffect(()=>{
+	// 	if (token) {
+	// 		getAllUsers();
+	// 	}	  
+	// },[token])
+	
   return (
     <div className='flex flex-col py-2 overflow-auto'>
-      {users.map((item,index)=>{
-        return <Conversation key = {index} item = {item} lastIndex={index === users.length - 1? false:true}/> 
-      })}
+		{(loading)? (<div className='loading loading-spinner'></div>):
+		conversations.map((item,index)=>{
+			return <Conversation key = {index} item = {item} lastIndex={index === conversations.length - 1? false:true}/> 
+		  })}
+      
   
     </div>
   )
