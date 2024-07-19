95732a84d171e74ae02bd4dfdd8cebde0b5f7c71
tanishqjain071003 <135233096+tanishqjain071003@users.noreply.github.com>
2024-07-19 15:15:49 +0530

    Update messageRoute.js

--- backend/routes/messageRoute.js
+++ backend/routes/messageRoute.js
@@ -1,11 +1,11 @@
-import {getMessages ,deleteMessage,sendMessage} from "../controllers/messageController.js";
+import {getMessages ,deleteMessage,unsendMessage} from "../controllers/messageController.js";
 import express from "express"
 import protectRoute from "../middleware/protectRoute.js";
 
 const messageRoute = express.Router()
 
 messageRoute.get('/:id',protectRoute,getMessages);
-messageRoute.post('/delete/:_id/',protectRoute,deleteMessage);
+messageRoute.post('/unsend/:_id/',protectRoute,unsendMessage);
 messageRoute.post('/send/:id',protectRoute, sendMessage);
 
 
