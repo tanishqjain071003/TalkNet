b3d00dd6e648bbb409ed7bae1ff52910554a0c6b
tanishqjain071003 <135233096+tanishqjain071003@users.noreply.github.com>
2024-07-21 01:34:35 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,21 @@
-import { useState } from 'react'
 import Login from "./pages/Login/Login.jsx"
 import './App.css'
 import Signup from './pages/Signup/Signup.jsx'
 import Home from './pages/Home/Home.jsx'
 import {Route,Routes} from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='p-4 h h-screen flex items-center justify-center'>
+      <div className=' flex justify-center items-center'>
         <Routes>
           <Route path = '/' element = {<Home/>}/>
           <Route path = '/login' element = {<Login/>}/>
           <Route path = '/signup' element = {<Signup/>}/>
         </Routes>
-        
+        <Toaster />
       </div>
     </>
   )
