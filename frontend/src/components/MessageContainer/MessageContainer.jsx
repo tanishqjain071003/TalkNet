e5844ca4ad69edfb169ed201a7603145cc36e4b9
tanishqjain071003 <135233096+tanishqjain071003@users.noreply.github.com>
2024-07-21 01:37:13 +0530

    Update MessageContainer.jsx

--- frontend/src/components/MessageContainer/MessageContainer.jsx
+++ frontend/src/components/MessageContainer/MessageContainer.jsx
@@ -5,28 +5,38 @@ import { TiMessages } from "react-icons/ti";
 import useConversation from '../../zustand/useConversation.js';
 import axios from 'axios';
 import { useAuthContext } from '../../context/AuthContext.jsx';
+import { useSocketContext } from '../../context/SocketContext.jsx';
+import ProfileContainer from './ProfileContainer.jsx';
 
 const MessageContainer = () => {
 
-	
+	const {url} = useAuthContext();
+	const [showProfile,setShowProfile] = useState(false);
 	const {selectedConversation, setSelectedConversation} = useConversation();
 	const noChatSelected = selectedConversation===null ? true : false;
-	
+	const {onlineUsers} = useSocketContext();
 	useEffect(()=>{
 		setSelectedConversation(null)
 	},[])
 
   return (
-	<div className='md:min-w-[450px] flex flex-col'>
+	<div className='flex-grow w-full md:w-2/3 flex flex-col justify-center h-full'>
     {
 		noChatSelected ?  (<NoChatSelected/>):
 		(
 			<>
-			<div className='bg-slate-500 px-4 py-2 mb-2'>
-				<span className='label-text'>To:</span> <span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
-			</div>
-			<Messages />
-			<MessageInput />
+				{!showProfile ? <>
+								<div onClick = {() => setShowProfile(!showProfile)} className='flex cursor-pointer bg-gradient-to-r from-emerald-900 to-cyan-700 h-14 items-center px-4 py-2 mb-2'>
+									<hr />
+									<div className='w-10'>
+										<img className = "rounded-full" src={url+'/images/'+selectedConversation.image} alt={selectedConversation.fullName}/>	
+									</div>
+									<span className='my-1 mx-4 text-white font-bold'>{selectedConversation.fullName}</span>
+								</div>
+									<Messages />
+									<MessageInput />
+								</>
+			: <ProfileContainer selectedConversation = {selectedConversation} setShowProfile = {setShowProfile}/>}
 			</>
 		)}
 	</div>
