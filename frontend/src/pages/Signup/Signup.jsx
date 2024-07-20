be0d3d0b060cb519962596670f67c9c06bb8d2a1
tanishqjain071003 <135233096+tanishqjain071003@users.noreply.github.com>
2024-07-21 01:35:19 +0530

    Update Signup.jsx

--- frontend/src/pages/Signup/Signup.jsx
+++ frontend/src/pages/Signup/Signup.jsx
@@ -3,7 +3,7 @@ import axios from "axios"
 import toast from 'react-hot-toast'
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuthContext } from '../../context/AuthContext'
-import upload_img from "../../assets/upload_area.png"
+import upload_img from "/Users/tanishqjain/Desktop/TalkNet/frontend/src/assets/upload_area.png"
 
 const Signup = () => {
   const {url} = useAuthContext();
@@ -46,59 +46,59 @@ const onSignup = async(e)=>{
 
   return (
     <>
-      <div className='flex flex-col items-center justify-center min-w-96 mx-auto' >
-        <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-          <h1 className='text-3xl font-semibold text-center text-gray-300'>
-            Login
-            <span className='text-blue-500'> TalkNet</span>
+      <div className='flex flex-col items-center justify-center min-w-96 mx-auto mt-24 shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)]' >
+        <div className='w-full p-6 rounded-lg bg-gradient-to-r from-emerald-900 to-cyan-700'>
+          <h1 className='text-stone-900 text-3xl text-bold font-extrabold text-center'>
+            Sign Up
+            <span className='text-green-400'> TalkNet</span>
           </h1>
         
 
         <form onSubmit={onSignup}>
           <div>
             <label className='label p-2'>
-              <span className="text-base label-text">Full Name</span>
+              <span className="text-base text-slate-950 font-semibold label-text">Full Name</span>
             </label>
-            <input required type="text" placeholder='Enter Full Name' className='w-full input input-bordered h-10'  value = {inputs.fullName} onChange={(e) => setInputs({...inputs, fullName:e.target.value})}/>
+            <input required type="text" placeholder='Enter Full Name' className='input input-bordered input-info w-full max-w-xs'  value = {inputs.fullName} onChange={(e) => setInputs({...inputs, fullName:e.target.value})}/>
           </div>
           
           <div>
             <label className='label p-2'>
-              <span className="text-base label-text">Username</span>
+              <span className="text-base text-slate-950 font-semibold label-text">Username</span>
             </label>
-            <input required type="text" placeholder='Enter Username' className='w-full input input-bordered h-10'  value = {inputs.username} onChange={(e) => setInputs({...inputs, username:e.target.value})}/>
+            <input required type="text" placeholder='Enter Username' className='input input-bordered input-info w-full max-w-xs'  value = {inputs.username} onChange={(e) => setInputs({...inputs, username:e.target.value})}/>
           </div>
 
           <div>
             <label className='label p-2'>
-              <span className="text-base label-text">Password</span>
+              <span className="text-base text-slate-950 font-semibold label-text">Password</span>
             </label>
-            <input required type="password" placeholder='Enter Password' className='w-full input input-bordered h-10'  value = {inputs.password} onChange={(e) => setInputs({...inputs, password:e.target.value})}/>
+            <input required type="password" placeholder='Enter Password' className='input input-bordered input-info w-full max-w-xs'  value = {inputs.password} onChange={(e) => setInputs({...inputs, password:e.target.value})}/>
           </div>
 
           <div>
             <label className='label p-2'>
-              <span className="text-base label-text">Confirm Password</span>
+              <span className="text-base text-slate-950 font-semibold label-text">Confirm Password</span>
             </label>
-            <input required type="password" placeholder='Confirm Password' className='w-full input input-bordered h-10'  value = {inputs.confirmPassword} onChange={(e) => setInputs({...inputs, confirmPassword:e.target.value})}/>
+            <input required type="password" placeholder='Confirm Password' className='input input-bordered input-info w-full max-w-xs'  value = {inputs.confirmPassword} onChange={(e) => setInputs({...inputs, confirmPassword:e.target.value})}/>
           </div>
 
           <div className='flex'>
             <div className='form-control'>
               <label className={`label gap-2 cursor-pointer ${inputs.gender === "Male" ? "selected":""}`}>
-                <span className='label-text'>Male</span>
+                <span className='text-base text-slate-950 font-semibold label-text'>Male</span>
                 <input  type='checkbox' className='checkbox border-slate-900' checked={inputs.gender === "Male"} onChange={() => handleCheckboxChange("Male")} />
               </label>
             </div>
             <div className='form-control'>
               <label className={`label gap-2 cursor-pointer ${inputs.gender === "Female" ? "selected":""}`}>
-                <span className='label-text'>Female</span>
+                <span className='text-base text-slate-950 font-semibold label-text'>Female</span>
                 <input  type='checkbox'  className='checkbox border-slate-900'checked={inputs.gender === "Female"} onChange={() => handleCheckboxChange("Female")} />
               </label>
             </div>
           </div>
           <div className='w-24 mb-2'>
-                    <p className='label-text'>Upload image</p>
+                    <p className='text-sm text-slate-950 font-semibold label-text'>Upload image</p>
                     <label htmlFor="image">
                         <img src={!image ? upload_img: URL.createObjectURL(image)} alt="" />
                     </label>
@@ -106,12 +106,12 @@ const onSignup = async(e)=>{
             </div>
 
 
-					<Link to ='/login' className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block' href='#'>
+					<Link to ='/login' className='text-base hover:text-zinc-700 duration-300 text-slate-950 font-semibold label-text' href='#'>
 						Already have an account?
 					</Link>
 
           <div>
-            <button className='btn btn-block btn-sm mt-2'>
+            <button className='btn btn-active mt-2 btn-accent w-full rounded-3xl text-base'>
               Sign in
             </button>
           </div>
