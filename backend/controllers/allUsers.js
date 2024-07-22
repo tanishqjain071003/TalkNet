4274807ea7e4af7ca4e9dbb27715987af6a78215
tanishqjain071003 <135233096+tanishqjain071003@users.noreply.github.com>
2024-07-23 02:25:26 +0530

    Update allUsers.js

--- backend/controllers/allUsers.js
+++ backend/controllers/allUsers.js
@@ -1,8 +1,7 @@
 import userModel from '../models/userModel.js'
-export const allUsers = async (req,res)=>{
+const allUsers = async (req,res)=>{
 
     try {
-
         const loggedInUserId = req.user.id
         const filteredUsers = await userModel.find({_id:{$ne : loggedInUserId}}).select("-password")
 
@@ -12,6 +11,26 @@ export const allUsers = async (req,res)=>{
         console.log(error);
         res.json({success:false,message:"Error"})
     }
-
-
 }
+const getFriends = async (req,res)=>{
+
+    try {
+        const loggedInUserId = req.user.id
+        const user = await userModel.findOne({_id:loggedInUserId}).select("-password")
+
+        if(user){
+            let friends = user.friends;
+            console.log(user.friends);
+    
+            res.json({success:true, friends});
+        }
+        else{
+            res.json({success:true})
+        }
+        
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
+}
+export {allUsers,getFriends};
