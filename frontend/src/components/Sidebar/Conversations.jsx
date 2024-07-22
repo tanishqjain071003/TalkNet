48f6b9c206cbc0d904438f8d105be4178b068630
tanishqjain071003 <135233096+tanishqjain071003@users.noreply.github.com>
2024-07-23 02:29:52 +0530

    Update Conversations.jsx

--- frontend/src/components/Sidebar/Conversations.jsx
+++ frontend/src/components/Sidebar/Conversations.jsx
@@ -1,37 +1,27 @@
 import {React,useState,useEffect} from 'react'
 import axios from "axios"
 import Conversation from './Conversation.jsx'
-import useGetConversations from '../../hooks/useGetConversations.js'
+import useGetFriends from '../../hooks/useGetFriends.js'
 
 const Conversations = () => {
 
-	// const url = "http://localhost:4000";
-	// const token = localStorage.getItem("token");
 
-	// const getAllUsers = async ()=>{
-	// 	const response = await axios({
-    //                     method: 'get',
-    //                     url: url + '/api/users',
-    //                     headers: {token}, 
-    //                   });
-	// 	if(response.data.success){
-	// 		setUsers(response.data.filteredUsers);
-	// 	}
-	// }
-	const {loading,conversations} = useGetConversations();
-
-
+	const {loading,friends} = useGetFriends();
+	const length = friends.length
 	// useEffect(()=>{
 	// 	if (token) {
 	// 		getAllUsers();
 	// 	}	  
 	// },[token])
-	
+	if(loading){
+		return <span className="loading loading-spinner loading-md"></span>
+	}
   return (
     <div className='flex flex-col py-2 overflow-scroll'>
-		{conversations.map((item,index)=>{
-			return <Conversation key = {index} item = {item} lastIndex={index === conversations.length - 1? false:true}/> 
-		  })}
+		{friends.map((item,index)=>{
+			return <Conversation key = {index} item = {item} lastIndex={index === friends.length - 1? false:true}/> 
+		})}
+		{!length && <p className='text-xl text-slate-300'>Add friends to start talking</p>}
     </div>
   )
 }
