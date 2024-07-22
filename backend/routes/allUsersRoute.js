e2e7a4e31db9f23bd2c8a78441395aa2d34fe585
tanishqjain071003 <135233096+tanishqjain071003@users.noreply.github.com>
2024-07-23 02:24:50 +0530

    Update allUsersRoute.js

--- backend/routes/allUsersRoute.js
+++ backend/routes/allUsersRoute.js
@@ -1,9 +1,10 @@
-import { allUsers } from "../controllers/allUsers.js";
+import { allUsers,getFriends } from "../controllers/allUsers.js";
 import express from "express"
 import protectRoute from "../middleware/protectRoute.js";
 
 const allUsersRoute = express.Router()
 
 allUsersRoute.get('/',protectRoute,allUsers);
+allUsersRoute.get('/getFriends',protectRoute,getFriends);
 
 export default allUsersRoute
