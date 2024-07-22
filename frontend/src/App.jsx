69407cbaaa5c42b1381b48bd083b3c4ea3a47f8f
tanishqjain071003 <135233096+tanishqjain071003@users.noreply.github.com>
2024-07-23 02:26:10 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
           <Route path = '/' element = {<Home/>}/>
           <Route path = '/login' element = {<Login/>}/>
           <Route path = '/signup' element = {<Signup/>}/>
+          <Route path = '/users' element = {<SearchBox/>}/>
         </Routes>
         <Toaster />
       </div>
