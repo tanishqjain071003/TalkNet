f0a9390dd7fe3b693a4ff4fca61f6e4e07a5e3e8
tanishqjain071003 <135233096+tanishqjain071003@users.noreply.github.com>
2024-07-24 00:44:54 +0530

    Update Messages.jsx

--- frontend/src/components/MessageContainer/Messages.jsx
+++ frontend/src/components/MessageContainer/Messages.jsx
@@ -18,7 +18,7 @@ const Messages = () => {
 
 
   return (
-    <div className='px-4 flex-1 overflow-auto'>
+    <div className='px-4 flex-1 overflow-auto rounded-3xl  border-zinc-800 border-1 m-2 p-2 shadow-[0px_0px_19px_2px_#48bb78]'>
       {!loading &&
 				messages.length > 0 &&
 				messages.map((message) => (
