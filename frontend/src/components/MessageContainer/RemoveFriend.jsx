17157be940af99e351d5857a9b88b062067cdaed
tanishqjain071003 <135233096+tanishqjain071003@users.noreply.github.com>
2024-07-24 00:40:29 +0530

    Add files via upload

---
+++ frontend/src/components/MessageContainer/RemoveFriend.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import toast from 'react-hot-toast'
+import axios from 'axios'
+import { useAuthContext } from '../../context/AuthContext'
+
+const RemoveFriend = ({selectedConversation}) => {
+    const {url} = useAuthContext();
+    const token = localStorage.getItem("token")
+
+    const removeFriend = async () => {
+        try {
+            const response = await axios({
+                method:"get",
+                url:url+`/api/user/removeFriend/${selectedConversation._id}`,
+                headers:{token},
+            })
+            console.log(response.data.success);
+            if(response.data.success){
+                toast.success(response.data.message);
+            }
+        } catch (error) {
+            toast.error(error.message);
+        }
+    };
+  return (
+    <div className="mx-8 flex justify-start">
+    <button onClick = {removeFriend} className="btn text-sm h-2 mt-auto btn-outline btn-error">Remove from friend list</button>
+  </div>
+  )
+}
+
+export default RemoveFriend
