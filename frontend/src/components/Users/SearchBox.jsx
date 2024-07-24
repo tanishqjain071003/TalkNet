d5de44e5342456c24a6f2650ca3ca88db1f49460
tanishqjain071003 <135233096+tanishqjain071003@users.noreply.github.com>
2024-07-24 14:22:30 +0530

    Update SearchBox.jsx

--- frontend/src/components/Users/SearchBox.jsx
+++ frontend/src/components/Users/SearchBox.jsx
@@ -7,8 +7,10 @@ import { IoMdArrowRoundBack } from "react-icons/io";
 const SearchBox = () => {
   return (
     <div className='w-full h-full p-12 bg-gradient-to-r from-emerald-900 to-cyan-700 md:flex-row sm:h-[780px] md:h-[780px]'>
-        <SearchInput/>
-        <SearchResults/>
+        <div className = 'overflow-auto'>
+          <SearchInput/>
+          <SearchResults/>
+        </div>
         <NavLink to = '/'>
           <div className='font-bold cursor-pointer mt-auto ml-4 mb-6' >
             <IoMdArrowRoundBack className='mt-auto'/>
