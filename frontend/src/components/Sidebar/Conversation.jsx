b2381a094479e1e86d9b097372b3185ae20598fa
tanishqjain071003 <135233096+tanishqjain071003@users.noreply.github.com>
2024-07-26 01:36:27 +0530

    Update Conversation.jsx

--- frontend/src/components/Sidebar/Conversation.jsx
+++ frontend/src/components/Sidebar/Conversation.jsx
@@ -14,14 +14,14 @@ const Conversation = ({lastIndex,item}) => {
   
   return (
     <>
-    <div onClick = {()=> setSelectedConversation(item)}className={`flex h-20 gap-2 my-2 items-center hover:bg-slate-800 rounded-3xl p-2 py-1 cursor-pointer ${isSelectedConversation?"bg-gradient-to-r from-gray-700 to-gray-800":""}`}>
+    <div onClick = {()=> setSelectedConversation(item)}className={`flex max-[640px]:h-16 h-20 gap-2 my-2 items-center hover:bg-slate-800 rounded-3xl p-2 py-1 cursor-pointer ${isSelectedConversation?"bg-gradient-to-r from-gray-700 to-gray-800":""}`}>
         <div className={`avatar ${isOnline ? "online":""}`}>
- 			    <div className='w-16 rounded-full '>
+ 			    <div className='w-16 max-[640px]:w-8 rounded-full '>
  						<img src={url+'/images/'+image} alt={item.fullName}/>
  					</div>
  			</div>
             <div className='flex flex-col justify-between'>
-                    <p className='font-bold text-gray-200 '>{item.fullName}</p>
+                    <p className='font-bold max-[640px]:text-xs text-gray-200 '>{item.fullName}</p>
             </div>
         </div>
       <div>
