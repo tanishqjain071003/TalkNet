278c451ee7073ceda47fd8e7ba217bfa931a902c
tanishqjain071003 <135233096+tanishqjain071003@users.noreply.github.com>
2024-07-26 01:29:46 +0530

    Update Login.jsx

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -25,8 +25,8 @@ export const Login = () => {
     }
   }
   return (
-    <div className='flex items-center justify-center rounded-xl bg-gradient-to-r from-slate-900 to-slate-700 mt-52 shadow-[0px_0px_15px_5px_#4a5568]'>
-      <div className='flex flex-col items-center justify-center min-w-96 h-full mt-auto mx-auto'>
+    <div className='flex items-center justify-center rounded-xl bg-gradient-to-r from-slate-900 to-slate-700 mt-52 shadow-[0px_0px_15px_5px_#4a5568] max-[640px]:flex-col'>
+      <div className='flex flex-col items-center justify-center min-w-96 h-full mt-auto mx-auto max-[640px]:items-center max-[640px]:justify-center max-[640px]:flex-row'>
         <div className='w-full flex flex-col p-6 items-center justify-center'>
           <h1 className='text-slate-100 text-3xl font-medium text-center'>
             Login
@@ -46,14 +46,14 @@ export const Login = () => {
               </label>
               <input type="password"  className='input input-bordered w-full max-w-xs' value = {data.password} onChange = {(e) => setData({...data,password:e.target.value})} placeholder='Enter password'/>
             </div>
-            <div>
+            <div className = 'max-[620px]:flex max-[620px]:items-center max-[620px]:justify-center'>
             <button className="btn btn-active btn-accent hover:bg-slate-700 hover:border-gray-600 text-slate-100 bg-gray-800 border-gray-400 w-52 mt-4 rounded-3xl text-base">Log In</button>
             </div>
           </form>
         </div>
       </div>
       
-      <div className='flex flex-col items-center  justify-start mr-8 gap-4'>
+      <div className='flex flex-col items-center  justify-start mr-8 gap-4 max-[620px]:mb-10'>
         <p className = 'text-slate-100 text-3xl font-medium text-center'>New Here?</p>
         <p className='text-xs text-slate-100 font-medium label-text w-44'>Sign up to connect with people all over the world!</p>
         <Link to= "/signup" className=''>
